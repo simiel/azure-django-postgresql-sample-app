6d95e9ef3c96f828b1349a1645100d1ac20aa6ed
Simiel <samensah036@st.ug.edu.gh>
2023-04-03 10:28:06 +0000

    Corrected inconsistent settings

--- manage.py
+++ manage.py
@@ -8,17 +8,7 @@ from dotenv import load_dotenv
 
 def main():
     """Run administrative tasks."""
-    # If WEBSITE_HOSTNAME is defined as an environment variable, then we're running on Azure App Service
-
-    # Only for Local Development - Load environment variables from the .env file
-    if 'WEBSITE_HOSTNAME' not in os.environ:
-        print("Loading environment variables for .env file")
-        load_dotenv('./.env')
-
-    # When running on Azure App Service you should use the production settings.
-    settings_module = "azureproject.production" if 'WEBSITE_HOSTNAME' in os.environ else 'azureproject.settings'
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
-
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ppl_backend.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -32,3 +22,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
